229a8fc07a5591f7cc90cc043e941118fce964da
Erfan Hasanzadeh <er.hasanzadeh@gmail.com>
2023-02-07 02:11:13 +0330

    add load more funcionality

--- src/pages/vendors/VendorsPage.tsx
+++ src/pages/vendors/VendorsPage.tsx
@@ -1,29 +1,43 @@
-import React, { PropsWithChildren, useEffect } from 'react';
+import React, { PropsWithChildren, useCallback, useEffect, useState } from 'react';
 import { shallowEqual } from 'react-redux';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { getVendorsFetch } from './vendorsSlice';
 import styles from './VendorsPage.module.scss'
 import { Vendor } from './vendor';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 interface OwnProps { }
 
 type Props = PropsWithChildren<OwnProps>;
 
 export function VendorsPage(props: Props) {
+    const [page, setPage] = useState<number>(0)
     const dispatch = useAppDispatch();
     const loading = useAppSelector(state => state.vendors.isLoading)
     const vendors = useAppSelector(state => state.vendors.vendors, shallowEqual)
     useEffect(() => {
-        dispatch(getVendorsFetch(0))
-    }, [])
-    if (loading) {
-        return <>Loading...</>
-    }
+        dispatch(getVendorsFetch(page))
+    }, [page])
+
+    const loadMore = useCallback(() => {
+        setPage(prevPage => prevPage + 1)
+    }, [setPage])
+
     return (
-        <div className={styles.wrapper}>
-            {loading ? <>Loading...</> : vendors.map(vendor => (
-                <Vendor vendor={vendor} key={vendor.title} />
-            ))}
-        </div>
+        <InfiniteScroll
+            dataLength={vendors.length}
+            next={loadMore}
+            hasMore={true}
+            loader={null}
+            endMessage={<p>You reached end!</p>}
+        >
+            <div className={styles.wrapper}>
+                {vendors.map(vendor => (
+                    <Vendor vendor={vendor} key={vendor.title} />
+                ))}
+                {loading ? <>Loading...</> : null}
+            </div>
+        </InfiniteScroll>
+
     )
 }
