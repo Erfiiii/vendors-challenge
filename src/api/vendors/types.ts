d2e434a56d9e3cc31e1183ed0f025d027a9c84f4
Erfan Hasanzadeh <er.hasanzadeh@gmail.com>
2023-02-06 12:51:11 +0330

    add axios and api

---
+++ src/api/vendors/types.ts
@@ -0,0 +1,10 @@
+
+export type Vendor = {
+    title: string;
+    description: string;
+    logo: string;
+    backgroundImage: string;
+    rate: number;
+    isExpress: boolean;
+    deliveryPrice: string;
+}
---
+++ src/api/vendors/vendors.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { Vendor } from './types';
+
+export const getVendors = async (page: number): Promise<Vendor[]> => {
+    try {
+        const res = await axios.get(`https://snappfood.ir/mobile/v3/restaurant/vendors-list?page=${page}&page_size=10&lat=35.754&long=51.328`)
+        return res.data.data.finalResult.filter((item: { type: 'VENDOR' | 'TEXT', data: any }) => item.type === 'VENDOR').map((item: { data: any }) => ({
+            title: item.data.title,
+            description: item.data.description,
+            logo: item.data.defLogo,
+            backgroundImage: item.data.backgroundImage,
+            rate: item.data.rate,
+            isExpress: item.data.isZFExpress,
+            deliveryPrice: item.data.deliveryFee
+        }))
+    } catch (error) {
+        throw error
+    }
+}
