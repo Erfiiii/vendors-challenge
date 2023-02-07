eb13b01586b42ca5d6436302cfd0e43cbb6f448a
Erfan Hasanzadeh <er.hasanzadeh@gmail.com>
2023-02-07 13:04:59 +0330

    fix

--- src/redux/saga.ts
+++ src/redux/saga.ts
@@ -1,12 +1,11 @@
-import { Action } from "@redux-saga/types";
-import { call, takeEvery, put, takeLatest } from "redux-saga/effects";
+import { call, put, takeLatest } from "redux-saga/effects";
 import { getVendors } from "../api/vendors";
 import { Vendor } from "../api/vendors/types";
 import { getVendorsFailure, getVendorsFetch, getVendorsSuccess } from "../pages/vendors/vendorsSlice";
 
 
 
-export function* fetchDataSaga(action: {
+export function* fetchVendorsSaga(action: {
     type: typeof getVendorsFetch.type,
     payload: number
 }) {
@@ -19,5 +18,5 @@ export function* fetchDataSaga(action: {
 }
 
 export default function* rootSaga() {
-  yield takeLatest(getVendorsFetch.type, fetchDataSaga);
+  yield takeLatest(getVendorsFetch.type, fetchVendorsSaga);
 }
